deaf2f5cef54b1a90ad3a387678534d06712f274
Chen Liu <liuchenhts@gmail.com>
2019-07-30 22:20:16 +1000

    header,footer,dashboard component works

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  // { path: 'detail/:id', component: HeroDetailComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
